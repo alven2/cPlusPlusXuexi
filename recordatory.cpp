a7682edd990a49de7c4e857f4223d24d2b4a4420
alven2 <alvenqiuzhang2@gmail.com>
2026-02-07 04:28:06 +0800

    update recordatory.cpp

--- recordatory.cpp
+++ recordatory.cpp
@@ -372,6 +372,9 @@ for (int i = 1; i <= column; i++) {
 
 
 
+// Memory adress, basicamente la direccion de un elemento en codigos, para mostarlo se usa &.
+std::string broo = "bro";
+std::cout << &broo << '\n';
 
 
 
